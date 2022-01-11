dbb92c75e887dab01a3a315b29d4a3a18c74236b
lonperman <lonperman@gmail.com>
2022-01-10 20:46:35 -0500

    Respondiendo json y modificando el struct de user

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 func main(){
 	server := NewServer(":3000")
-	server.Handle("/", HandleRoot)
-	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth(),Logging()))
+	server.Handle("GET","/", HandleRoot)
+	server.Handle("POST","/create", PostRequest)
+	server.Handle("POST","/user", UserPostRequest)
+	server.Handle("POST","/api", server.AddMiddleware(HandleHome, CheckAuth(),Logging()))
 	server.Listen()
 }
